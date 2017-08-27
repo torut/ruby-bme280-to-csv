896e81e7d93b9515c33a7dd0743db45922f31d33
Toru TAMURA <torut@ttsoft.jp>
2017-08-27 15:15:32 +0900

    add x-axis label.

--- export_graph.rb
+++ export_graph.rb
@@ -9,12 +9,31 @@ today_data_file = File.expand_path('./data/' + today + '.csv', __dir__)
 require "csv"
 dataset = CSV.table(today_data_file, { :headers => ['time', 'temperature', 'humidity', 'pressure'] })
 
-# グラフ画像を作成する
+# グラフ画像を作成する(横幅 900px)
 require 'gruff'
-g = Gruff::Line.new
+g = Gruff::Line.new(900)
+
+# グラフのポイントサイズ
+g.dot_radius = 2
+
+# グラフのデータ
 g.title = today + 'の気温グラフ'
-g.data '気温', dataset[:temperature]
-g.data '湿度', dataset[:humidity]
+g.data '気温', dataset[:temperature], '#C70044' # 赤系
+g.data '湿度', dataset[:humidity],    '#008A83' # 緑系
+
+# 時間ラベルの間隔(8項目表示する)
+x_axis_interval = (dataset[:time].size / 8).to_i
+
+# X軸のラベルを入れる
+require 'time'
+x_axis = {}
+dataset[:time].each_index do |i|
+  if i % x_axis_interval == 0
+    t = Time.parse(dataset[:time][i])
+    x_axis[i] = t.strftime('%H:%M') # HH:MM に変更
+  end
+end
+g.labels = x_axis
 
 # 日本語フォントを指定すると日本語が反映される
 g.font = '/usr/share/fonts/opentype/ipafont-gothic/ipag.ttf'
